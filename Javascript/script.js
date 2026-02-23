edbc28a92090a6a125840e611d558db643894a6a
ezazdesign <ezazdesign77@gmail.com>
2026-02-24 04:27:46 +0600

    responsive done and left border add on the card

--- Javascript/script.js
+++ Javascript/script.js
@@ -49,6 +49,7 @@ cards.forEach(function (card) {
         statusBadge.className =
             "rounded font-medium px-3 py-2 bg-green-100 text-green-700 w-fit text-sm";
         updateUI();
+        card.style.borderLeft = "6px solid #10B981";
     });
 
     // REJECTED BUTTON
@@ -58,6 +59,7 @@ cards.forEach(function (card) {
         statusBadge.className =
             "rounded font-medium px-3 py-2 bg-red-100 text-red-700 w-fit text-sm";
         updateUI();
+        card.style.borderLeft = "6px solid #EF4444";
     });
 
     // DELETE BUTTON
